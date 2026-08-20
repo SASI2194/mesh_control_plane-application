34c47c11c39b0d0d458f74025393626523bf69fd
Sasikumar <sasikumare2094@gmail.com>
2026-08-20 11:51:52 +0530

    fix(zenoh): populate all 9 mesh node endpoints (.65 to .73) in connect.endpoints and set listen.endpoints to tcp/0.0.0.0:7447

--- mesh_node.py
+++ mesh_node.py
@@ -41,7 +41,7 @@ from dashboard.server import start_dashboard_background, DATA_PROVIDER
 from core.network_models import MeshSample
 
 
-PEER_CONFIG = "/home/nvidia/ws_rmw_zenoh/src/rmw_zenoh-humble/rmw_zenoh_cpp/config/tcp/zenoh_peer_tcp.json5"
+PEER_CONFIG = "config/zenoh/zenoh_router_tcp.json5"
 
 
 class ROSPublisherBridge:
